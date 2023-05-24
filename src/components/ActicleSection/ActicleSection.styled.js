1d0dc3af7e8678565deba26db704dcce82e8cc6a
Sergey <sergey.cherenkov96@gmail.com>
2023-05-24 22:45:51 +0300

    gradient to article sec

--- src/components/ActicleSection/ActicleSection.styled.js
+++ src/components/ActicleSection/ActicleSection.styled.js
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 
 export const ActicleSectionWrapper = styled.section`
+  .container {
+    position: relative;
+  }
+
   .section-title {
     flex-direction: column;
   }
@@ -32,6 +36,38 @@ export const ActicleSectionWrapper = styled.section`
 
     @media (min-width: 1200px) {
       gap: 68px;
+    }
+  }
+
+  .gradient-bg {
+    width: 600px;
+    height: 100%;
+    position: absolute;
+    top: -150px;
+    left: -150px;
+
+    z-index: -999;
+    background-image: radial-gradient(
+      circle,
+      #82ffe8,
+      rgba(255, 255, 255, 0) 50%
+    );
+    background-repeat: no-repeat;
+    background-position: -40px -75px;
+    opacity: 0.5;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-image: radial-gradient(
+        circle,
+        #c8ff82,
+        rgba(255, 255, 255, 0) 55%
+      );
+      background-repeat: no-repeat;
+      background-position: -30px 140px;
     }
   }
 `;
