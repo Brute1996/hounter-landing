b76e50fbba93a5952d80670c6b92ad40be1b1fbd
Sergey <sergey.cherenkov96@gmail.com>
2023-05-24 11:35:03 +0300

    fix primary button styles

--- src/components/common/PrimaryButton/PirmaryButton.styled.js
+++ src/components/common/PrimaryButton/PirmaryButton.styled.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-export const PrimaryLinkStyle = styled.a`
+export const PrimaryButtonStyle = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -17,27 +17,16 @@ export const PrimaryLinkStyle = styled.a`
   line-height: 1.57;
   color: #fff;
 
-  @media (min-width: 768px) {
-    padding: 13px 16px;
+  transition: scale 100ms ease-in-out;
+
+  &:hover,
+  &:focus {
+    scale: 1.05;
   }
-`;
 
-export const PrimaryButtonStyle = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 2;
-
-  background: #10b981;
-  border-radius: 32px;
-  border: none;
-  padding: 8px 8px;
-
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 1.57;
-  color: #fff;
+  &:active {
+    scale: 0.85;
+  }
 
   @media (min-width: 768px) {
     padding: 13px 16px;
--- src/components/common/PrimaryButton/PrimaryButton.jsx
+++ src/components/common/PrimaryButton/PrimaryButton.jsx
@@ -1,14 +1,14 @@
-import { PrimaryButtonStyle, PrimaryLinkStyle } from "./PirmaryButton.styled"
+import { PrimaryButtonStyle } from "./PirmaryButton.styled"
 
 export const PrimaryButton = ({ buttonType, linkHref, children, gap, handleClick }) => {
 
 
     
     if (buttonType) {
-        return <PrimaryButtonStyle className="primary-button" onClick={handleClick} style={{gap}} type={buttonType}>{children}</PrimaryButtonStyle>
+        return <PrimaryButtonStyle as="button" className="primary-button" onClick={handleClick} style={{gap}} type={buttonType}>{children}</PrimaryButtonStyle>
     }
 
     if (linkHref) {
-        return <PrimaryLinkStyle className="primary-button" style={{gap}} href={linkHref}>{children}</PrimaryLinkStyle>
+        return <PrimaryButtonStyle as="a" className="primary-button" style={{gap}} href={linkHref}>{children}</PrimaryButtonStyle>
     }
 }
