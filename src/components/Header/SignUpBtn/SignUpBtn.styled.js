b91a5fbbcec27786e22f6c39515275da9af9c142
Sergey <sergey.cherenkov96@gmail.com>
2023-05-25 13:40:43 +0300

    sig up animation added

--- src/components/Header/SignUpBtn/SignUpBtn.styled.js
+++ src/components/Header/SignUpBtn/SignUpBtn.styled.js
@@ -16,4 +16,16 @@ export const SignUpBtnStyle = styled.button`
   font-weight: 600;
   font-size: 14px;
   line-height: 1.57;
+
+  transition: scale 100ms ease-in-out;
+
+  &:hover,
+  &:focus {
+    scale: 1.1;
+    outline: 3px dashed #047857;
+  }
+
+  &:active {
+    scale: 0.9;
+  }
 `;
